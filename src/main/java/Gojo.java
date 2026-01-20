202c242f8182c7e7dd86dd955fabf99926944b82
Niranjan Vijay <niranjan.vijay346@gmail.com>
2026-01-20 16:57:48 +0800

    Level-0: Rename Duke to Gojo and add greeting and exit messages

---
+++ src/main/java/Gojo.java
@@ -0,0 +1,18 @@
+public class Gojo {
+    public static void main(String[] args) {
+
+        String logo =
+                "  ██████   ██████      ██   ██████  \n"
+                        + " ██        ██   ██     ██   ██    ██ \n"
+                        + " ██  ████  ██   ██     ██   ██    ██ \n"
+                        + " ██    ██  ██   ██     ██   ██    ██ \n"
+                        + "  ██████   ██████   ██ ██    ██████  \n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Gojo 🤞🏻.");
+        System.out.println("Consider this your domain of organisation.");
+        System.out.println("What would you like to do?");
+        System.out.println("Bye, until next time - Stay Limitless");
+
+    }
+}
